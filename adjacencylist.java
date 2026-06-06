ff28f94ca7f4ca0d85d8fa3bca60caf8a5acea16
Palak Narang <163756539+palaknarang05@users.noreply.github.com>
2026-06-06 23:55:12 +0530

    Add files via upload

---
+++ adjacencylist.java
@@ -0,0 +1,58 @@
+package graphs;
+import java.util.*;
+
+public class adjacencylist {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s,int d,int w){
+            this.src=s;
+            this.dest=d;
+            this.wt=w;
+        }
+    }
+    public static void main(String[] args){
+        int v=5;
+        ArrayList<Edge>[] graph= new ArrayList[v]; // null-> empty arry list
+
+        for(int i=0;i<v;i++){
+            graph[i]=new ArrayList<>();
+        }
+        //vertex 0
+        graph[0].add(new Edge(0, 1, 5));
+
+        //vertex-1
+
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2 , 1));
+        graph[1].add(new Edge(1, 3 , 3));
+
+        //vertex-2
+
+        graph[2].add(new Edge(2, 1 , 1));
+        graph[2].add(new Edge(2, 3 , 1));
+        graph[2].add(new Edge(2, 4 , 2));
+
+
+        //vertex-3
+        graph[3].add(new Edge(3, 2 , 1));
+        graph[3].add(new Edge(3, 1 , 3));
+
+        //vertex4
+
+        graph[4].add(new Edge(4, 2 , 1));
+
+        //2's neighboir
+        for(int i=0;i<graph[2].size();i++){
+            Edge e=graph[2].get(i);
+            System.out.println(e.dest);
+        }
+
+
+
+    }
+
+    
+}
